d865fb2eb454c08ab50b841a71a67d2700772a4b
Bryan Nissen <bcn3@njit.edu>
2017-10-18 16:54:50 -0400

    Update index.php

--- index.php
+++ index.php
@@ -8,8 +8,8 @@ $hostname = 'sql.njit.edu';
 $dsn = "mysql:host=$hostname;dbname=$username";
 
 try {
-	$conn = new PDO($dsn, $username, $password);
-	echo "Connected successfully<br>";
+    $conn = new PDO($dsn, $username, $password);
+    echo "Connected successfully<br>";
 } catch(PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
 }
