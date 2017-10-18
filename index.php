daebdb280d01e220a38f43a8e6f65a6459c0a76f
shimshamz <sm2292@njit.edu>
2017-10-18 19:15:44 -0400

    Week 7 Assignment Commit

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 
 echo "<h1>PDO demo!</h1>";
-$username = '<ucid here>';
-$password = '<password here>';
-$hostname = 'sql.njit.edu';
+$username = 'sm2292';
+$password = 'cadmium5';
+$hostname = 'sql2.njit.edu';
 
 $dsn = "mysql:host=$hostname;dbname=$username";
 
@@ -15,4 +15,24 @@ try {
 }
 $conn = null;
 
+
+// Assignment Begins
+
+$id = 6
+
+$query = 'SELECT * FROM accounts
+		  WHERE id < :id';
+$statement = $conn->prepare($query);
+$statement->bindValue(':id', $id); 
+$statement->execute();
+$statement->closerCursor();
 ?>
+
+<?php foreach ($accounts as $account) { ?>
+<table>
+	<tr>
+		<td><?php echo $account['id']; ?></td>
+		<td><?php echo $account['email']; ?></td>
+	</tr>
+</table>
+<?php } ?>
