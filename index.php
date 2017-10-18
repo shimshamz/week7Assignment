83443ac886c06349008dd0c864ad5c2078a0c96d
Bryan Nissen <bryannissen@gmail.com>
2017-10-18 10:57:33 -0400

    Commit Example

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+echo "<h1>PDO demo!</h1>";
+$username = '<ucid here>';
+$password = '<password here>';
+$hostname = 'sql.njit.edu';
+
+$dsn = "mysql:host=$hostname;dbname=$username";
+
+try {
+	$conn = new PDO($dsn, $username, $password);
+	echo "Connected successfully<br>";
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+$conn = null;
+
+?>
