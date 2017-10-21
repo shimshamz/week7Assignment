18c13e21cd1794744ebe4fd45836956c3a3de478
shimshamz <sm2292@njit.edu>
2017-10-21 15:41:32 -0400

    Final Commit

--- index.php
+++ index.php
@@ -10,31 +10,30 @@ $dsn = "mysql:host=$hostname;dbname=$username";
 try {
     $conn = new PDO($dsn, $username, $password);
     echo "Connected successfully<br>";
+    $id = 6;
+	$query = 'SELECT * FROM accounts WHERE id < :id';
+	$statement = $conn->prepare($query);
+	$statement->bindValue(':id', $id); 
+	$statement->execute();
+	$accounts = $statement->fetchAll();
+	$count = $statement->rowCount();
+	echo "Results: $count<br>";
+	
 } catch(PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
 }
 $conn = null;
-
-// Assignment Begins
-
-$id = 6;
-
-$query = 'SELECT * FROM accounts WHERE id < :id';
-$statement = $conn->prepare($query);
-$statement->bindValue(':id', $id); 
-$statement->execute();
-$accounts = $statement->fetchAll();
-$statement->closerCursor();
-
-$count = $statement->rowCount();
-echo "Results: <br>";
 ?>
 
-<?php foreach ($accounts as $account) { ?>
 <table border="1">
+	<th>ID</th>
+	<th>Email</th>
+<?php foreach ($accounts as $account) { ?>
 	<tr>
-		<td><?php echo $account['id']; ?></td>
-		<td><?php echo $account['email']; ?></td>
+		<td><?php echo $account['id'];?></td>
+		<td><?php echo $account['email'];?></td>
 	</tr>
+<?php }
+$statement->closerCursor();
+?>
 </table>
-<?php } ?>
